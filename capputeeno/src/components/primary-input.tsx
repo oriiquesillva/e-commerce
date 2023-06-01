c112a622a7aac6dcb9286732a4fc71c51964555b
Luiz Souza <lsillva.dev@gmail.com>
2023-06-01 10:18:38 -0300

    feat: creating header

---
+++ capputeeno/src/components/primary-input.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { SearchIcon } from "./search-icon";
+import { InputHTMLAttributes } from "react";
+
+
+export const PrimaryInput = styled.input`
+    width: 352px;
+    border-radius: 8px;
+    padding: 10px 16px;
+
+    background-color: var(--bg-secondary);
+    border: none;
+ 
+    font-family: 'inherit';
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    color: var(--text-dark);
+
+`
+
+export const InputContainer = styled.div`
+    position: relative;
+    width: 352px;
+
+    svg{
+        position: absolute;
+        top:50%;
+        right: 20px;
+        transform: translateY(-50%);
+
+    }
+`
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function PrimaryInputWSearchIcon(props: InputProps){
+    return(
+        <InputContainer>
+            <PrimaryInput {...props}/>
+            <SearchIcon/>
+        </InputContainer>
+    )
+}
