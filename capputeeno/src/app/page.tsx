329503bd3bf86252360aa860210a9fe24ca47135
Luiz Souza <lsillva.dev@gmail.com>
2023-06-03 13:40:07 -0300

    feat: created product page

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,29 +1,26 @@
 "use client"
 
+import { DefaultPageLayout } from '@/components/default-page-layout'
 import { FilterBar } from '@/components/filter-bar'
 import { ProductList } from '@/components/products-list'
 import styled from 'styled-components'
 
 const PageWrapper = styled.main`
-    min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 12px 24px;
-    background-color: var(--bg-primary);
-
-    @media(min-width: ${props => props.theme.desktopBreakpoint}){
-      padding: 34px 160px;
-    }
+  
 `
 
 
 export default function Home() {
   return (
+    <DefaultPageLayout>
       <PageWrapper>
-        <FilterBar/>
-        <ProductList/>
-    </PageWrapper>
+          <FilterBar/>
+          <ProductList/>
+      </PageWrapper>
+    </DefaultPageLayout>
 
   )
 }
--- capputeeno/src/types/product.ts
+++ capputeeno/src/types/product.ts
@@ -2,6 +2,14 @@ export interface Product {
     id: string,
     name: string,
     price_in_cents: number,
-    image_url: string
+    image_url: string,
+    description?: string,
+    category?: string
     
+}
+
+export interface ProductFetchResponse {
+    data: {
+        Product: Product
+    }
 }
